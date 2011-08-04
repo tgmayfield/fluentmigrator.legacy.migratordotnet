c5ef8a660088f8e03229c036f3812cbe3ecdab9f
Thomas G Mayfield <tgmayfield@gmail.com>
2011-08-04 13:08:40 -0600

    R# points out I wasn't using those parameters. Ouch.

--- src/FluentMigrator.Legacy.MigratorDotNet/ForeignKeyExtensions.cs
+++ src/FluentMigrator.Legacy.MigratorDotNet/ForeignKeyExtensions.cs
@@ -9,7 +9,7 @@ namespace FluentMigrator.Legacy.MigratorDotNet
 	{
 		public static void AddForeignKey(this ITransformationProvider database, string name, string sourceTable, string sourceColumn, string destTable, string destColumn)
 		{
-			database.AddForeignKey(name, sourceColumn, new[] { sourceColumn }, destTable, new[] { destColumn });
+			database.AddForeignKey(name, sourceTable, new[] { sourceColumn }, destTable, new[] { destColumn });
 		}
 		public static void AddForeignKey(this ITransformationProvider database, string name, string sourceTable, string[] sourceColumns, string destTable, string[] destColumns)
 		{
@@ -22,7 +22,9 @@ namespace FluentMigrator.Legacy.MigratorDotNet
 
 		public static void RemoveForeignKey(this ITransformationProvider database, string table, string keyName)
 		{
-			new DeleteExpressionRoot(database.Context).ForeignKey(keyName);
+			new DeleteExpressionRoot(database.Context)
+				.ForeignKey(keyName)
+				.OnTable(table);
 		}
 	}
 }
